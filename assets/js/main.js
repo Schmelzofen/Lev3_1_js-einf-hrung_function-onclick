77a2e9c92133b7538b60652347035b75c3459d2d
Schmelzofen <sertac.oezdemir@gmail.com>
2021-10-26 14:49:21 +0200

    Colorchanger wow

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+function ballOne(){
+    console.log("test")
+    document.getElementById("roy").style.backgroundColor="green"
+    document.getElementById("roy").style.color="purple"
+}
+
+function ballTwo(){
+    document.getElementById("roy").style.backgroundColor="rgb(71,61,116)"
+    document.getElementById("roy").style.color="yellow"
+}
+
+function ballThree(){
+    document.getElementById("roy").style.backgroundColor="rgb(2,0,33)"
+    document.getElementById("roy").style.color="rgb(211,87,0)"
+}
+
+function ballFour(){
+    document.getElementById("roy").style.backgroundColor="rgb(12,0,74)"
+    document.getElementById("roy").style.color="cyan"
+}
+

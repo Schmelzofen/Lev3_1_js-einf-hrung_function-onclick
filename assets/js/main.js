b492c3b22f57b9e027256a70560e1e871fec93ed
Schmelzofen <sertac.oezdemir@gmail.com>
2021-10-27 09:43:16 +0200

    deg fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,22 @@
-let bdy = document.getElementById("roy")
 let h1c = document.getElementById("bob")
 
 function ballOne(){
-    bdy.style.backgroundColor="green"
+    document.body.style.backgroundColor="green"
     h1c.style.color="purple"
 }
 
 function ballTwo(){
-    bdy.style.backgroundColor="rgb(71,61,116)"
+    document.body.style.backgroundColor="rgb(71,61,116)"
     h1c.style.color="yellow"
 }
 
 function ballThree(){
-    bdy.style.backgroundColor="rgb(2,0,33)"
+    document.body.style.backgroundColor="rgb(2,0,33)"
     h1c.style.color="rgb(211,87,0)"
 }
 
 function ballFour(){
-    bdy.style.backgroundColor="rgb(12,0,74)"
+    document.body.style.backgroundColor="rgb(12,0,74)"
     h1c.style.color="cyan"
 }
 
